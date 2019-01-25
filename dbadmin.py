c88492cf1179cec7508dbac8c80e830d2e7eba24
simon <simon.c.slator@outlook.com>
2019-01-25 07:25:38 +0000

    Initial commit

---
+++ dbadmin.py
@@ -0,0 +1,218 @@
+import sys
+
+import MySQLdb as MariaDB
+from PyQt5 import QtWidgets
+
+from dbadmin_ui import Ui_QuickAdmin
+
+
+class QuickAdminForm(Ui_QuickAdmin):
+    def __init__(self, ui):
+        Ui_QuickAdmin.__init__(self)
+        self.setupUi(ui)
+        self.conn = MariaDB.connect(db='catalogue', use_unicode=True, charset='utf8', read_default_file='~/.my.cnf')
+
+
+        self.populateArtCombo()
+        self.populateAlbCombo()
+        self.populateTypeCombo()
+        self.populateSourceCombo()
+        #self.populateLabelCombo()
+        self.populateAlbumDetails()
+        self.setTrackHeaders()
+
+        self.cmbArt.currentIndexChanged.connect(self.populateAlbCombo)
+        self.cmbAlb.currentIndexChanged.connect(self.populateAlbumDetails)
+        self.tableTracks.itemChanged.connect(self.updateTrackData)
+        self.cmbType.currentIndexChanged.connect(self.updateAlbumType)
+        self.cmbLabel.currentIndexChanged.connect(self.updateAlbumLabel)
+        self.cmbSource.currentIndexChanged.connect(self.updateSource)
+        self.txtEdition.textChanged.connect(self.updateEdition)
+        self.cmdSaveLabel.clicked.connect(self.addNewLabel)
+
+        self.labelComboRefreshing = False
+
+
+    def updateEdition(self):
+        alb_id = self.getSelectedAlbum()
+        if self.txtEdition.text() != "":
+            sql = "UPDATE album SET comments = %s WHERE albumid = %s;"
+            cursor = self.conn.cursor()
+            cursor.execute(sql, (self.txtEdition.text(), alb_id, ))
+            self.conn.commit()
+
+    def updateAlbumType(self):
+        alb_id = self.getSelectedAlbum()
+        typeid = int(self.cmbType.itemData(self.cmbType.currentIndex()))
+        sql = "UPDATE album SET albumtypeid = %s WHERE albumid = %s;"
+        cursor = self.conn.cursor()
+        cursor.execute(sql, (typeid, alb_id, ))
+        self.conn.commit()
+
+    def updateSource(self):
+        alb_id = self.getSelectedAlbum()
+        typeid = int(self.cmbSource.itemData(self.cmbSource.currentIndex()))
+        sql = "UPDATE album SET sourceid = %s WHERE albumid = %s;"
+        cursor = self.conn.cursor()
+        cursor.execute(sql, (typeid, alb_id, ))
+        self.conn.commit()
+
+    def updateAlbumLabel(self):
+        if not self.labelComboRefreshing:
+            alb_id = self.getSelectedAlbum()
+            labelid = int(self.cmbLabel.itemData(self.cmbLabel.currentIndex()))
+            sql = "UPDATE album SET labelid = %s WHERE albumid = %s;"
+            cursor = self.conn.cursor()
+            cursor.execute(sql, (labelid, alb_id, ))
+            self.conn.commit()
+
+    def updateTrackData(self, item):
+        alb_id = self.getSelectedAlbum()
+        row = self.tableTracks.currentRow()
+        col = self.tableTracks.currentColumn()
+        newdata = item.text()
+
+        if row >= 0:
+            disc = int(self.tableTracks.item(row, 0).text())
+            track = int(self.tableTracks.item(row, 1).text())
+
+            field = None
+
+            if col == 2:
+                field = "tracktitle"
+            elif col == 4:
+                field = "bonustrack"
+
+            if field is not None:
+                sql = "UPDATE track SET {} = %s WHERE albumid = %s AND disc = %s AND track = %s;".format(field)
+                cursor = self.conn.cursor()
+                cursor.execute(sql, (newdata, alb_id, disc, track, ))
+                self.conn.commit()
+
+    def setTrackHeaders(self):
+        headers = ["Disc", "Track", "Title", "Length", "Bonus"]
+        widths = [40, 40, 400, 60, 40]
+
+        for x in range(0, 5):
+            self.tableTracks.setHorizontalHeaderItem(x, QtWidgets.QTableWidgetItem(headers[x]))
+            self.tableTracks.setColumnWidth(x, widths[x])
+
+    def getSelectedArtist(self):
+        art_id = int(self.cmbArt.itemData(self.cmbArt.currentIndex()))
+        return art_id
+
+    def getSelectedAlbum(self):
+        if self.cmbAlb.count() > 0:
+            alb_id = int(self.cmbAlb.itemData(self.cmbAlb.currentIndex()))
+            return alb_id
+        else:
+            return None
+
+    @staticmethod
+    def makeSafe(text):
+        return text.replace("'", "''")
+
+    def labelExists(self, label):
+        c = self.conn.cursor()
+        rowcount = c.execute("SELECT * from label where label = '{}';".format(self.makeSafe(label)))
+        return rowcount > 0
+
+    def addNewLabel(self):
+        label = self.txtAddNewLabel.text()
+        if not self.labelExists(label) and not label == '':
+            sql = "INSERT INTO label(label) values ('{}')".format(self.makeSafe(label))
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+            self.conn.commit()
+            self.populateLabelCombo()
+            self.txtAddNewLabel.clear()
+
+    def setAlbumType(self):
+        alb_id = self.getSelectedAlbum()
+        c = self.conn.cursor()
+        c.execute("SELECT albumtypeid, sourceid, comments "
+                  "from album where albumid=%s;", (alb_id, ))
+        typerow = c.fetchone()
+        self.cmbType.setCurrentIndex(self.cmbType.findData(typerow[0]))
+        self.cmbSource.setCurrentIndex(self.cmbSource.findData(typerow[1]))
+        # self.cmbLabel.setCurrentIndex(self.cmbLabel.findData(typerow[2]))
+        self.txtEdition.setText(typerow[2])
+
+    def populateArtCombo(self):
+        c = self.conn.cursor()
+        c.execute("SELECT distinct artist.artistid, artistname from artist "
+                  "inner join albumartist on albumartist.artistid = artist.artistid "
+                  "order by artistname;")
+        artlist = c.fetchall()
+
+        for a in artlist:
+            self.cmbArt.addItem(a[1], a[0])
+
+    def populateAlbCombo(self):
+        art_id = self.getSelectedArtist()
+        if art_id is not None:
+            c = self.conn.cursor()
+            c.execute("SELECT album.albumid, album from album inner join albumartist on album.albumid = albumartist.albumid "
+                      "WHERE artistid={} order by album;".format(art_id))
+            alblist = c.fetchall()
+            self.cmbAlb.clear()
+            for a in alblist:
+                self.cmbAlb.addItem(a[1], a[0])
+
+    def populateTypeCombo(self):
+        c = self.conn.cursor()
+        c.execute("SELECT * from albumtype order by albumtypeid;")
+        typelist = c.fetchall()
+
+        for a in typelist:
+            self.cmbType.addItem(a[1], a[0])
+
+    def populateLabelCombo(self):
+        current_value = self.cmbLabel.itemData(self.cmbLabel.currentIndex())
+        self.labelComboRefreshing = True
+        self.cmbLabel.clear()
+        c = self.conn.cursor()
+        c.execute("SELECT distinct labelid, label from label order by label;")
+        artlist = c.fetchall()
+
+        if artlist is not None:
+            for a in artlist:
+                self.cmbLabel.addItem(a[1], a[0])
+
+        self.cmbLabel.setCurrentIndex(self.cmbLabel.findData(current_value))
+        self.labelComboRefreshing = False
+
+    def populateSourceCombo(self):
+        c = self.conn.cursor()
+        c.execute("SELECT * from source order by sourceid;")
+        typelist = c.fetchall()
+
+        for a in typelist:
+            self.cmbSource.addItem(a[1], a[0])
+
+    def clearTrackTable(self):
+        while self.tableTracks.rowCount() > 0:
+            self.tableTracks.removeRow(0)
+
+    def populateAlbumDetails(self):
+        alb_id = self.getSelectedAlbum()
+        if alb_id is not None:
+            self.setAlbumType()
+            c = self.conn.cursor()
+            c.execute("SELECT disc, track, tracktitle, length, bonustrack from track "
+                      "WHERE albumid={} order by disc, track;".format(alb_id))
+            tracklist = c.fetchall()
+            self.clearTrackTable()
+            for t in tracklist:
+                rowpos = self.tableTracks.rowCount()
+                self.tableTracks.insertRow(rowpos)
+                for x in range(0, 5):
+                    self.tableTracks.setItem(rowpos, x, QtWidgets.QTableWidgetItem(str(t[x])))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    dialog = QtWidgets.QDialog()
+    prog = QuickAdminForm(dialog)
+    dialog.show()
+    sys.exit(app.exec_())
---
+++ dbadmin_ui.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'quick_admin.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_QuickAdmin(object):
+    def setupUi(self, dbadmin):
+        dbadmin.setObjectName("QuickAdmin")
+        dbadmin.resize(640, 641)
+        self.tableTracks = QtWidgets.QTableWidget(dbadmin)
+        self.tableTracks.setGeometry(QtCore.QRect(10, 90, 621, 341))
+        self.tableTracks.setAlternatingRowColors(True)
+        self.tableTracks.setColumnCount(5)
+        self.tableTracks.setObjectName("tableTracks")
+        self.tableTracks.setRowCount(0)
+        self.cmbArt = QtWidgets.QComboBox(dbadmin)
+        self.cmbArt.setGeometry(QtCore.QRect(60, 20, 281, 22))
+        self.cmbArt.setObjectName("cmbArt")
+        self.cmbAlb = QtWidgets.QComboBox(dbadmin)
+        self.cmbAlb.setGeometry(QtCore.QRect(60, 50, 281, 22))
+        self.cmbAlb.setObjectName("cmbAlb")
+        self.label = QtWidgets.QLabel(dbadmin)
+        self.label.setGeometry(QtCore.QRect(10, 20, 47, 13))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(dbadmin)
+        self.label_2.setGeometry(QtCore.QRect(10, 50, 47, 13))
+        self.label_2.setObjectName("label_2")
+        self.cmbType = QtWidgets.QComboBox(dbadmin)
+        self.cmbType.setGeometry(QtCore.QRect(120, 440, 211, 22))
+        self.cmbType.setObjectName("cmbType")
+        self.label_3 = QtWidgets.QLabel(dbadmin)
+        self.label_3.setGeometry(QtCore.QRect(10, 440, 101, 16))
+        self.label_3.setObjectName("label_3")
+        self.cmbSource = QtWidgets.QComboBox(dbadmin)
+        self.cmbSource.setGeometry(QtCore.QRect(120, 470, 211, 22))
+        self.cmbSource.setObjectName("cmbSource")
+        self.label_4 = QtWidgets.QLabel(dbadmin)
+        self.label_4.setGeometry(QtCore.QRect(10, 470, 101, 16))
+        self.label_4.setObjectName("label_4")
+        self.txtEdition = QtWidgets.QLineEdit(dbadmin)
+        self.txtEdition.setGeometry(QtCore.QRect(120, 560, 211, 20))
+        self.txtEdition.setObjectName("txtEdition")
+        self.label_6 = QtWidgets.QLabel(dbadmin)
+        self.label_6.setGeometry(QtCore.QRect(10, 560, 101, 16))
+        self.label_6.setObjectName("label_6")
+        self.label_5 = QtWidgets.QLabel(dbadmin)
+        self.label_5.setGeometry(QtCore.QRect(10, 500, 101, 16))
+        self.label_5.setObjectName("label_5")
+        self.cmbLabel = QtWidgets.QComboBox(dbadmin)
+        self.cmbLabel.setGeometry(QtCore.QRect(120, 500, 211, 22))
+        self.cmbLabel.setObjectName("cmbLabel")
+        self.label_7 = QtWidgets.QLabel(dbadmin)
+        self.label_7.setGeometry(QtCore.QRect(10, 530, 101, 16))
+        self.label_7.setObjectName("label_7")
+        self.txtAddNewLabel = QtWidgets.QLineEdit(dbadmin)
+        self.txtAddNewLabel.setGeometry(QtCore.QRect(120, 530, 211, 20))
+        self.txtAddNewLabel.setObjectName("txtAddNewLabel")
+        self.cmdSaveLabel = QtWidgets.QPushButton(dbadmin)
+        self.cmdSaveLabel.setGeometry(QtCore.QRect(340, 530, 101, 24))
+        self.cmdSaveLabel.setObjectName("cmdSaveLabel")
+        self.txtAddNewLabel.setEnabled(False)
+        self.cmdSaveLabel.setEnabled(False)
+        self.retranslateUi(dbadmin)
+        QtCore.QMetaObject.connectSlotsByName(dbadmin)
+
+    def retranslateUi(self, QuickAdmin):
+        _translate = QtCore.QCoreApplication.translate
+        QuickAdmin.setWindowTitle(_translate("QuickAdmin", "Quick Admin"))
+        self.label.setText(_translate("QuickAdmin", "Artist:"))
+        self.label_2.setText(_translate("QuickAdmin", "Title:"))
+        self.label_3.setText(_translate("QuickAdmin", "Type:"))
+        self.label_4.setText(_translate("QuickAdmin", "Source"))
+        self.label_6.setText(_translate("QuickAdmin", "Edition Notes:"))
+        self.label_5.setText(_translate("QuickAdmin", "Label"))
+        self.label_7.setText(_translate("QuickAdmin", "Add New Label:"))
+        self.cmdSaveLabel.setText(_translate("QuickAdmin", "Save + Refresh"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    dbdmin = QtWidgets.QDialog()
+    ui = Ui_QuickAdmin()
+    ui.setupUi(dbdmin)
+    dbdmin.show()
+    sys.exit(app.exec_())
+
